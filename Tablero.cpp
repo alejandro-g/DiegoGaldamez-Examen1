0609aeefe73cb673335a4a034e6f48db5d8f2ae2
Diego Alejandro Galdamez Tinoco <diego.galdamez@unitec.edu>
2016-11-11 17:15:08 -0600

    message

--- Tablero.cpp
+++ Tablero.cpp
@@ -1,21 +1,41 @@
 #include "Tablero.h"
 #include <iostream>
 
+using namespace std;
+
 int matriz[11][11];
+int filas = 0;
+int columnas = 0;
+int i;
+int j;
 
 void Tablero::crear(){
-  for (int i = 0; i < 11; i++) {
+  for (i = 0; i < 11; i++) {
+    for(j = 0; j < 11; j++) {
+    }
   }
-  for (int j = 0; j < 11; j++) {
-  }
-  matriz[i][j] = 0;
 }
 
-void Tablero::impirmir(){
-    for (int i = 0; i < 11; i++) {
-      for (int j = 0; j < 11; j++) {
+void Tablero::imprimir(){
+    for (i = 0; i < 11; i++) {
+      for (j = 0; j < 11; j++) {
         cout << "[" << matriz[i][j] << "]";
       }
       cout << endl;
     }
   }
+
+void Tablero::eliminar(){
+  for (int i = 0; i < matriz[i]; i++) {
+    delete[] matriz[i];
+    matriz[i] = NULL;
+  }
+
+  delete[]matriz;
+
+  cout << "Borrando..." << endl;
+}
+void Tablero::movimiento(){
+
+}
+}
--- mainExamen.cpp
+++ mainExamen.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 int posicion = 0;
 int posicion2 = 0;
-int palyer1;
-int player2;
 
 int main(){
   Tablero* instancia = new Tablero();
